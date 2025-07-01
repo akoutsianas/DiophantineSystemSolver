fafb1bccb112df04f1052fdf81e89618e7624e13
Angelos Koutsianas <koutsis.jr@gmail.com>
2025-07-01 14:00:19 +0300

    Make code compatible with the paper. Fix minor bugs.

--- diophantine_system_solver.py
+++ diophantine_system_solver.py
@@ -3,7 +3,7 @@ from sage.all import RealField, sqrt, log, floor, ZZ, exp
 
 class DiophantineSystem:
 
-    def __init__(self, f, d1, d2, x0=1, prec=100, alpha1=0.5, eps=0.5, verbose=0):
+    def __init__(self, f, d1, d2, prec=100, verbose=0):
         """
         We want to solve the Diophantine system f(x)=d_1y_1^n, x=d_2y_2^n. We give an upper bound n0 of n and
         the solutions when n > n0.
@@ -11,10 +11,7 @@ class DiophantineSystem:
         :param f: a polynomial with integer coefficients
         :param d1: an integer
         :param d2: an integer
-        :param x0: an integer. We assume that |x|>=x0
         :param prec: the precision for the real number computations
-        :param alpha1: a value of a' if a0d2^k/d1 is an integer
-        :param eps: an upper bound of the difference between a and a'
         :param verbose: 0, 1, 2
         """
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -27,15 +24,17 @@ class DiophantineSystem:
         self.k = f.degree()
         self._ais = self._compute_ais()
         self._cis = self._compute_cis()
-        if self._ais[0] * self.d2**self.k < 0 or self.d1 < 0:
+        if (self._ais[0] * self.d2**self.k < 0) or (self.d1 < 0):
             raise ValueError(f"The conditions a0*d2^k > 0 and d1 > 0 are not satisfied!")
         self.k0 = self._compute_k0()
-        self._D = (self._ais[0] * self.d2**self.k) / self.d1
-        if self._D == 1:
+        self._thetan = (self._ais[0] * self.d2**self.k) / self.d1
+        if self._thetan == 1:
             raise ValueError("The quantity a0*d2^k/d1 is equal to 1.")
         self._theta1, self._theta0 = self._compute_thetas()
         self.m = self._compute_m()
         self._alpha1 = self._compute_alpha1()
+        self.x1 = self._compute_x1()
+        self.x2 = self._compute_x2()
 
     def set_verbose(self, verbose):
         # Map integers to logging levels
@@ -69,7 +68,7 @@ class DiophantineSystem:
         return cis
 
     def _compute_m(self):
-        r = self._ais[0] * self.d2**self.k / self.d1
+        r = self._thetan
         if r in ZZ:
             m = r + 0.5
         else:
@@ -78,32 +77,32 @@ class DiophantineSystem:
         return m
 
     def _compute_thetas(self):
-        if self._D > 1:
-            theta1 = sqrt(self._D)
+        if self._thetan > 1:
+            theta1 = self._RR(self._thetan) ** (1/3)
             theta0 = 1
         else:
             theta1 = 1
-            theta0 = sqrt(self._D)
+            theta0 = self._RR(self._thetan) ** (1/3)
         self.logger.debug(f"theta1: {theta1}, theta0: {theta0}")
         return theta1, theta0
 
     def _compute_c0_c1_c2(self, x0):
-        c0 = sum([abs(self._cis[self.k0 + i])/x0**i for i in range(self.k - self.k0 + 1)])
+        c0 = sum([abs(self._cis[i])/x0**(i - self.k0) for i in range(self.k0, self.k + 1)])
         self.logger.debug(f"c0: {c0}")
 
         if self.k == self.k0:
             c1 = abs(self.d2**self.k * self._ais[self.k])
         else:
-            c1 = abs(self._ais[self.k0]) - sum([abs(self._ais[self.k0+i+1])/x0**(i + 1) for i in range(self.k - self.k0)])
+            c1 = abs(self._ais[self.k0]) - sum([abs(self._ais[i])/x0**(i-self.k0) for i in range(self.k0+1, self.k+1)])
             c1 *= abs(self.d2**self.k)
         self.logger.debug(f"c1: {c1}")
 
-        c2 = exp(c1 / (self._theta0 * x0**(self.k0)))
+        c2 = exp(c0 / (self._theta0 * x0**self.k0))
         self.logger.debug(f"c2: {c2}")
         return c0, c1, c2
 
     def _compute_alpha1(self):
-        a1 = self.m - self._ais[0] * self.d2**self.k / self.d1
+        a1 = self.m - self._thetan
         self.logger.debug(f"a1: {a1}")
         return a1
 
@@ -112,9 +111,10 @@ class DiophantineSystem:
         self.logger.debug(f"alpha: {alpha}")
         return alpha
 
-    def _compute_x1(self, c1):
-        x1 = c1 * max(1/(1 - self._alpha1), 1/(self._theta1 - self._theta0))
-        x1 = x1**(1/self.k0)
+    def _compute_x1(self):
+        x1 = max(1/(1 - self._alpha1), 1/(self._theta1 - self._theta0))
+        x1 *= sum([abs(self._cis[i]) for i in range(self.k0, self.k+1)])
+        x1 = self._RR(x1)**(1/self.k0)
         self.logger.debug(f"x1: {x1}")
         return x1
 
@@ -131,12 +131,13 @@ class DiophantineSystem:
     def bound_n_for_x0(self, x0):
         c0, c1, c2 = self._compute_c0_c1_c2(x0)
         alpha = self._compute_alpha(x0, c0)
-        x1 = self._compute_x1(c1)
-        x2 = self._compute_x2()
-        if (x0 > x1) and (x0 > x2):
+        if (x0 > self.x1) and (x0 > self.x2):
             lam = self._compute_lam(c0, c1, c2, alpha)
-            num = log((self._ais[0] * self.d2**self.k) / (self.d1 * (self.m + alpha)))
+            num = log(self._RR((self._ais[0] * self.d2**self.k) / (self.d1 * (self.m + alpha))))
+            self.logger.debug(f"num: {num}")
             bound = floor(num / log(lam))
+            self.logger.debug(f"Bound from computations: {bound}")
+            bound = max(bound, 3)
             self.logger.info(f"The bound is {bound}")
         else:
             self.logger.warning(f"It holds x0 <= x1 or x0 <= x2")
